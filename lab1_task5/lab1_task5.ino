609487c50af8e6e9bf44ff9257931ea97dccba80
Hugo Groene <hugo.groene@student.uva.nl>
2023-06-08 15:01:16 +0200

    added

---
+++ lab1_task5/lab1_task5.ino
@@ -0,0 +1,39 @@
+  // -------------------------------------------
+  //
+  //  Poject: Lab1_task5
+  //  Group: 76
+  //  Students: Adam Dong, Hugo Groene
+  //  Date: juni 8 2023
+  //  ------------------------------------------
+
+  #include <Arduino_LSM6DS3.h>
+  
+  float x, y, z;  
+
+  // put your setup code here
+  void setup() {
+
+    // initialize serial port and wait for port to open:
+    Serial.begin(9600);
+
+    // wait for serial port to connect. Needed for native USB port only
+    while (!Serial) {}
+
+    if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+    }
+  }
+
+  // put your main code here
+  void loop() {
+    if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(x, y, z);
+
+        Serial.print(x);
+        Serial.print(',');
+        Serial.print(y);
+        Serial.print(',');
+        Serial.println(z);
+    }
+  }
